da182d32dc8d7b6676e40ce6e07408b789cb16f1
KrozFu <carlosuefagh@gmail.com>
2026-08-03 17:37:13 -0500

    feat(ui): setup core layout, styling and application entry point

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import { Login } from "./pages";
+import { Home } from "./pages";
+import { Vehicles } from "./pages/Vehicles";
+import { Header } from "./components";
+import { ProductDetails } from "./pages/ProductDetail";
+
+const WithHeader = () => (
+  <>
+    <Header />
+    <Outlet />
+  </>
+);
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route element={<WithHeader />}>
+            <Route path="/vehicles" element={<Vehicles />} />
+            <Route path="/details/:id" element={<ProductDetails />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
